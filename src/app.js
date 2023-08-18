e91cfb4a5984381cfe0e11a78459e3d56908b3f5
Paularicarte <115987177+Paularicarte@users.noreply.github.com>
2023-08-18 07:58:30 +0000

    Proyecto terminado con todo solucionado wiii, fotos coinciden con el who

--- src/app.js
+++ src/app.js
@@ -5,8 +5,9 @@ import "./assets/img/4geeks.ico";
 window.onload = () => {
   //write your code here
   document.querySelector("#btn").addEventListener("click", () => {
-    document.querySelector("#the-excuse").innerHTML = generateExcuse().excuse;
-    document.querySelector("#the-img").src = generateExcuse().imagen;
+    let excuseData = generateExcuse();
+    document.querySelector("#the-excuse").innerHTML = excuseData.excuse;
+    document.querySelector("#the-img").src = excuseData.imagen;
   });
 
   console.log("Hello Rigo from the console!");
@@ -16,11 +17,11 @@ let generateExcuse = () => {
   let img = [
     "src/assets/img/dog.jpg",
     "src/assets/img/grandma.jpg",
-    "src/assets/img/turtle.jpg",
-    "src/assets/img/dinosaur.jpg",
-    "src/assets/img/neighbors_cat.jpg",
+    "src/assets/img/tortuga.avif",
+    "src/assets/img/dino.gif",
+    "src/assets/img/gato.jpg",
     "src/assets/img/Wind.jpg",
-    "src/assets/img/computer.jpg",
+    "src/assets/img/ordenador.avif",
     "src/assets/img/lavadoraRota.jpg",
     "src/assets/img/sneezingPanda.png"
   ];
@@ -75,30 +76,35 @@ let generateExcuse = () => {
   let actionIndex = Math.floor(Math.random() * action.length);
   let whatIndex = Math.floor(Math.random() * what.length);
   let whenIndex = Math.floor(Math.random() * when.length);
-  let imgIndex;
-
-  // Usar una estructura condicional para definir el índice de imagen basado en el índice de "who"
-  if (whoIndex === 0) {
-    imgIndex = 0; // Índice de imagen correspondiente a "The dog"
-  } else if (whoIndex === 1) {
-    imgIndex = 1; // Índice de imagen correspondiente a "My grandma"
-  } else if (whoIndex === 2) {
-    imgIndex = 2; // Índice de imagen correspondiente a "turtle"
-  } else if (whoIndex === 3) {
-    imgIndex = 3; // Índice de imagen correspondiente a "dinosaur"
-  } else if (whoIndex === 4) {
-    imgIndex = 4; // Índice de imagen correspondiente a "neighbors_cat"
-  } else if (whoIndex === 5) {
-    imgIndex = 5; // Índice de imagen correspondiente a "wind"
-  } else if (whoIndex === 6) {
-    imgIndex = 6; // Índice de imagen correspondiente a "computer"
-  } else if (whoIndex === 7) {
-    imgIndex = 7; // Índice de imagen correspondiente a "lavadoraRota"
-  } else if (whoIndex === 8) {
-    imgIndex = 8; // Índice de imagen correspondiente a "sneezing"
-  }
-
+  let imgIndex = whoIndex;
   let imgSource = img[imgIndex];
+
+  // let imgIndex = imageIndexMap[who[whoIndex]];
+  // if (imgIndex === undefined) {
+  //   imgIndex = 0; // Índice de imagen predeterminado
+  // }
+  // let imgSource = img[imgIndex];
+
+  // // Usar una estructura condicional para definir el índice de imagen basado en el índice de "who"
+  // if (whoIndex === 0) {
+  //   imgIndex = 0; // Índice de imagen correspondiente a "The dog"
+  // } else if (whoIndex === 1) {
+  //   imgIndex = 1; // Índice de imagen correspondiente a "My grandma"
+  // } else if (whoIndex === 2) {
+  //   imgIndex = 2; // Índice de imagen correspondiente a "turtle"
+  // } else if (whoIndex === 3) {
+  //   imgIndex = 3; // Índice de imagen correspondiente a "dinosaur"
+  // } else if (whoIndex === 4) {
+  //   imgIndex = 4; // Índice de imagen correspondiente a "neighbors_cat"
+  // } else if (whoIndex === 5) {
+  //   imgIndex = 5; // Índice de imagen correspondiente a "wind"
+  // } else if (whoIndex === 6) {
+  //   imgIndex = 6; // Índice de imagen correspondiente a "computer"
+  // } else if (whoIndex === 7) {
+  //   imgIndex = 7; // Índice de imagen correspondiente a "lavadoraRota"
+  // } else if (whoIndex === 8) {
+  //   imgIndex = 8; // Índice de imagen correspondiente a "sneezing"
+  // }
 
   return {
     excuse:
